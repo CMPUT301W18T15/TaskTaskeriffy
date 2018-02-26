db063a63fa3a3c98a2fa7fe45212445b991fb46f
chengze2 <35279545+chengze2@users.noreply.github.com>
2018-02-26 09:42:06 +0800

    unit test for TaskRequester and TaskProvider

--- app/src/androidTest/java/com/example/heesoo/myapplication/TaskProviderTest.java
+++ app/src/androidTest/java/com/example/heesoo/myapplication/TaskProviderTest.java
@@ -13,17 +13,29 @@ public class TaskProviderTest extends ActivityInstrumentationTestCase2 {
         super(TaskProvider.class);
     }
 
+    private String taskRequester = "requester1";
+    private String TaskName = "task1";
+    private String TaskDescription = "task description";
+
     public void testGetTasks(){
         TaskProvider taskprovider = new TaskProvider();
+
+        Task task = new Task(taskRequester, TaskName, TaskDescription, "Requested");
+        taskprovider.MyTasks.add(task);
+
         assertEquals(taskprovider.MyTasks, taskprovider.getTasks());
     }
 
     public void testGetAssignedTasks(){
         TaskProvider taskprovider = new TaskProvider();
+
+        Task task = new Task(taskRequester, TaskName, TaskDescription, "Assigned");
+        taskprovider.MyTasks.add(task);
+
         ArrayList<Task> assignedList = new ArrayList<Task>;
-        for(Task task : taskprovider.MyTasks){
-            if (!task.assignedProvider.isEmpty()){
-                assignedList.add(task);
+        for(Task tasks : taskprovider.MyTasks){
+            if (tasks.status = "Assigned"){
+                assignedList.add(tasks);
             }
         }
         assertEquals(assignedList, taskprovider.getAssignedTasks());
@@ -31,10 +43,14 @@ public class TaskProviderTest extends ActivityInstrumentationTestCase2 {
 
     public void testGetBiddedTasks(){
         TaskProvider taskprovider = new TaskProvider();
+
+        ask task = new Task(taskRequester, TaskName, TaskDescription, "Bidded");
+        taskprovider.MyTasks.add(task);
+
         ArrayList<Task> biddedList = new ArrayList<Task>;
-        for(Task task : taskprovider.MyTasks) {
-            if (!task.bids.isEmpty()) {
-                biddedList.add(task);
+        for(Task tasks : taskprovider.MyTasks) {
+            if (tasks.status = "Bidded") {
+                biddedList.add(tasks);
             }
         }
         assertEquals(biddedList, taskprovider.getBiddedTasks());
--- app/src/androidTest/java/com/example/heesoo/myapplication/TaskRequesterTest.java
+++ app/src/androidTest/java/com/example/heesoo/myapplication/TaskRequesterTest.java
@@ -14,17 +14,29 @@ public class TaskRequesterTest extends ActivityInstrumentationTestCase2 {
         super(TaskRequester.class);
     }
 
+    private String taskRequester = "requester1";
+    private String TaskName = "task1";
+    private String TaskDescription = "task description";
+
     public void testGetTasks(){
         TaskRequester taskrequester = new TaskRequester();
+
+        Task task = new Task(taskRequester, TaskName, TaskDescription, "Requested");
+        taskrequester.MyTasks.add(task);
+
         assertEquals(taskrequester.MyTasks, taskrequester.getTasks());
     }
 
     public void testGetAssignedTasks(){
         TaskRequester taskrequester = new TaskRequester();
+
+        Task task = new Task(taskRequester, TaskName, TaskDescription, "Assigned");
+        taskrequester.MyTasks.add(task);
+
         ArrayList<Task> assignedList = new ArrayList<Task>;
-        for(Task task : taskrequester.MyTasks){
-            if (!task.assignedProvider.isEmpty()){
-                assignedList.add(task);
+        for(Task tasks : taskrequester.MyTasks){
+            if (tasks.status = "Assigned"){
+                assignedList.add(tasks);
             }
         }
         assertEquals(assignedList, taskrequester.getAssignedTasks());
@@ -32,10 +44,14 @@ public class TaskRequesterTest extends ActivityInstrumentationTestCase2 {
 
     public void testGetBiddedTasks(){
         TaskRequester taskrequester = new TaskRequester();
+
+        ask task = new Task(taskRequester, TaskName, TaskDescription, "Bidded");
+        taskrequester.MyTasks.add(task);
+
         ArrayList<Task> biddedList = new ArrayList<Task>;
-        for(Task task : taskrequester.MyTasks) {
-            if (!task.bids.isEmpty()) {
-                biddedList.add(task);
+        for(Task tasks : taskrequester.MyTasks) {
+            if (tasks.status = "Bidded") {
+                biddedList.add(tasks);
             }
         }
         assertEquals(biddedList, taskrequester.getBiddedTasks());
