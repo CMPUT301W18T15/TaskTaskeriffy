236549acc99a7e752a7e62c5262583f465bc43b5
harry996 <wenchen@ualberta.ca>
2018-03-14 19:06:36 -0600

    viewbidlistActivity in progress

---
+++ app/src/main/java/com/example/heesoo/myapplication/ViewBidListActivity.java
@@ -0,0 +1,14 @@
+package com.example.heesoo.myapplication;
+// Harry
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewBidListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_bid_list);
+
+    }
+}
