16b24e11c877709dc22383260f53980e5b062bfb
chengze2 <chengze2@ualberta.ca>
2018-03-18 00:31:00 -0600

    fixed edit task minimum input length constraint

--- app/src/main/java/com/example/heesoo/myapplication/Requestor/RequestorEditTaskActivity.java
+++ app/src/main/java/com/example/heesoo/myapplication/Requestor/RequestorEditTaskActivity.java
@@ -1,16 +1,21 @@
 package com.example.heesoo.myapplication.Requestor;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.heesoo.myapplication.Constraints.TaskConstraints;
 import com.example.heesoo.myapplication.ElasticSearchControllers.ElasticSearchTaskController;
 import com.example.heesoo.myapplication.Entities.Task;
 import com.example.heesoo.myapplication.R;
+import com.example.heesoo.myapplication.SetCurrentUser.SetCurrentUser;
 
 
 /**
@@ -22,36 +27,67 @@ public class RequestorEditTaskActivity extends AppCompatActivity {
     private EditText descriptionEdit;
     private Button saveChangesButton;
     private Task task;
+    private EditText taskName;
+    private EditText taskDescription;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_task);
-        taskNameEdit = findViewById(R.id.taskNameEdit);
-        descriptionEdit = findViewById(R.id.descriptionEdit);
+        taskName = findViewById(R.id.taskNameEdit);
+        taskDescription = findViewById(R.id.descriptionEdit);
         saveChangesButton = findViewById(R.id.saveChangesButton);
 
         Intent intent = getIntent();
         task = (Task)intent.getSerializableExtra("TaskToEdit");
-        taskNameEdit.setText(task.getTaskName());
-        descriptionEdit.setText(task.getTaskDescription());
+        taskName.setText(task.getTaskName());
+        taskDescription.setText(task.getTaskDescription());
 
         saveChangesButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                String newTaskName = taskNameEdit.getText().toString();
-                String newTaskDescription = descriptionEdit.getText().toString();
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                int duration = Toast.LENGTH_SHORT;
 
-                task.setTaskName(newTaskName);
-                task.setTaskDescription(newTaskDescription);
+                setResult(RESULT_OK);
+                String name = taskName.getText().toString();
+                String description = taskDescription.getText().toString();
 
-                ElasticSearchTaskController.EditTask editTask =
-                        new ElasticSearchTaskController.EditTask();
-                editTask.execute(task);
-                Intent edited_task = new Intent(getApplicationContext(), RequestorShowTaskDetailActivity.class);
-                edited_task.putExtra("TaskEdited", task);
-                setResult(Activity.RESULT_OK, edited_task);
-                finish();
+                // Check if Required fields are entered
+                if (name.isEmpty()){
+                    CharSequence text = "Missing Required Fields";
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }else{
+                    // Save all the fields
+                    TaskConstraints taskConstraints = new TaskConstraints();
+                    if (taskConstraints.titleLength(name)){
+                        if (taskConstraints.descriptionLength(description)) {
+                            task.setTaskName(name);
+                            task.setTaskDescription(description);
+
+                            ElasticSearchTaskController.EditTask editTask =
+                                    new ElasticSearchTaskController.EditTask();
+                            editTask.execute(task);
+                            Intent edited_task = new Intent(getApplicationContext(), RequestorShowTaskDetailActivity.class);
+                            edited_task.putExtra("TaskEdited", task);
+                            setResult(Activity.RESULT_OK, edited_task);
+                            CharSequence text = "Saving Task";
+                            Toast toast = Toast.makeText(context, text, duration);
+                            toast.show();
+                            new Handler().postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    finish();
+                                }
+                            }, 1000);
+                        }else{
+                            Toast.makeText(getApplicationContext(),"Maximum length of Description (300 characters)", Toast.LENGTH_SHORT).show();
+                        }
+                    }else{
+                        Toast.makeText(getApplicationContext(),"Maximum length of Title (30 characters)", Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
         });
 
